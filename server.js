09af1f5b714301a45407024e8f83be651633370a
Ahamad-PC <ahamad.jedu@gmail.com>
2018-05-19 23:50:47 +0700

    2.Add Router

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express()
 
 
+app.get('/',(req,res)=>{
+    res.send('Root Page');
+})
+
+app.get('/member',(req,res)=>{
+    res.send('Member Page');
+})
+
+app.get('/order',(req,res)=>{
+    res.send('Order Page');
+})
+
+
 app.listen(8000, () => {
     console.log('ready on http://localhost:8000')
 })
